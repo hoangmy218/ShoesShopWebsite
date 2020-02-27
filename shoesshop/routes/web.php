6ca11d754e3a134c1f38ec9e54df951b4f5768c0
hoangmy218 <ngothihoangmy218@gmail.com>
2020-02-27 20:15:24 +0700

    create project cut layout admin_login_dashboard shop

---
+++ shoesshop/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//frontend hoạt động phía user
+Route::get('/','HomeController@index');
+
+
+//backend hoat dong phia server
+
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard', 'AdminController@show_dashboard');
+Route::post('/admin-dashboard', 'AdminController@dashboard');
